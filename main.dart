f19513de188272f567c739aac2998c9e2dd38b25
minal909 <89183363+minal909@users.noreply.github.com>
2022-05-02 23:23:23 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:grid_app/Model/create_grid.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CreateGrid(),
+    );
+  }
+}
+
+/*class MyHomePage extends StatefulWidget {
+ 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[*/
+             
